c76cdb524b3760f74eff4cebf8674f668da0a8b5
Marat Zaynutdinov <tsundokum@gmail.com>
2015-01-04 22:30:21 +0300

    image normalization (broken code)

--- data_prep.lua
+++ data_prep.lua
@@ -3,6 +3,8 @@ require 'lfs'
 
 TRAIN_DIR = [[/media/marat/MySSD/plankton/train]]
 
+CROP_SIZE = 64
+
 classes = {}
 for d in lfs.dir(TRAIN_DIR) do
     if string.sub(d, 1, 1) ~= '.' then
@@ -35,35 +37,75 @@ function iterateImages(imageProcessor)
     end
 end
 
-sz_w = {}
-sz_h = {}
-function saveHW(img, c, fn)
-    table.insert(sz_w, (#img[1])[1])
-    table.insert(sz_h, (#img[1])[2])
-    return 0
-end
-iterateImages(saveHW)
-mw = 0
-for i, s in pairs(sz_w) do
-    mw = math.max(s, mw)
-end
-print('max width: '..mw)
 
-mh = 0
-for i, s in pairs(sz_h) do
-    mh = math.max(s, mh)
+-----------------------------------------
+------- collect sizes of all pictures in training sample
+--sz_w = {}
+--sz_h = {}
+--function saveHW(img, c, fn)
+--    table.insert(sz_w, (#img[1])[1])
+--    table.insert(sz_h, (#img[1])[2])
+--    return 0
+--end
+--iterateImages(saveHW)
+--mw = 0
+--for i, s in pairs(sz_w) do
+--    mw = math.max(s, mw)
+--end
+--print('max width: '..mw)
+--
+--mh = 0
+--for i, s in pairs(sz_h) do
+--    mh = math.max(s, mh)
+--end
+--print('max heigh: '..mh)
+
+
+-------------------------------
+------- draw big pictures
+-------------------------------
+--function plotBig(img, c, fn)
+--    if (#img[1])[1] >= 410 then
+--        print(c..'/'..fn)
+--        image.display{image=img, legend=c}
+--    end
+--    return 0
+--end
+--iterateImages(plotBig)
+
+function d(img)
+    image.display(img)
 end
-print('max heigh: '..mh)
 
+function inverseImage(img)
+    return img:clone():fill(1.0):add(-img)
+end
 
-function plotBig(img, c, fn)
-    if (#img[1])[1] >= 410 then
-        print(c..'/'..fn)
-        image.display{image=img, legend=c}
+function normalizeImage(img)
+    local cimg = img:clone()
+    local h = (#cimg)[2]
+    local w = (#cimg)[3]
+
+    if w > CROP_SIZE or h > CROP_SIZE then
+        local xOffset = math.max(0, math.floor((w-CROP_SIZE)/2))
+        local yOffset = math.max(0, math.floor((h-CROP_SIZE)/2))
+        w = math.min(CROP_SIZE, w)
+        h = math.min(CROP_SIZE, h)
+        cimg = image.crop(cimg, 1 + xOffset, 1 + yOffset, w + xOffset, h + yOffset)
     end
-    return 0
+
+    local nimg = cimg:clone():resize(1, CROP_SIZE, CROP_SIZE):new():fill(1.0)
+    local xOffset = math.floor((CROP_SIZE-w)/2)
+    local yOffset = math.floor((CROP_SIZE-h)/2)
+    print('xOffset='..xOffset..'  yOffset='..yOffset..'  h='..h..'  w='..w)
+    nimg[{1, {1+yOffset, h+yOffset}, {1+xOffset,w+xOffset}}] = cimg
+    return nimg
 end
 
-iterateImages(plotBig)
+img = image.load[[/media/marat/MySSD/plankton/train/copepod_cyclopoid_copilia/30556.jpg]]
+--img = image.load[[/media/marat/MySSD/plankton/train/acantharia_protist/79447.jpg]]
+d(img)
+d(normalizeImage(img))
+d(img)
 
 
