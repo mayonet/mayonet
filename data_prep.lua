2caee728aa64d75bccda29ef2de5cb3723135bf1
Marat Zaynutdinov <tsundokum@gmail.com>
2015-01-05 11:31:33 +0300

    image normalization fixed

--- data_prep.lua
+++ data_prep.lua
@@ -77,10 +77,12 @@ function d(img)
     image.display(img)
 end
 
-function inverseImage(img)
-    return img:clone():fill(1.0):add(-img)
-end
+--function inverseImage(img)
+--    return img:clone():fill(1.0):add(-img)
+--end
 
+-----------------------
+-- crop image to 1xCROP_SIZExCROP_SIZE
 function normalizeImage(img)
     local cimg = img:clone()
     local h = (#cimg)[2]
@@ -91,8 +93,13 @@ function normalizeImage(img)
         local yOffset = math.max(0, math.floor((h-CROP_SIZE)/2))
         w = math.min(CROP_SIZE, w)
         h = math.min(CROP_SIZE, h)
-        cimg = image.crop(cimg, 1 + xOffset, 1 + yOffset, w + xOffset, h + yOffset)
+        print((1 + xOffset)..' '..(1 + yOffset)..' '..(w + xOffset)..' '..(h + yOffset)..' w='..w..' h='..h)
+        cimg = image.crop(cimg, 1 + xOffset, 1 + yOffset, w + xOffset + 1, h + yOffset + 1)
     end
+
+    local nh = (#cimg)[2]
+    local nw = (#cimg)[3]
+    print('new_h='..nh..' new_w='..nw)
 
     local nimg = cimg:clone():resize(1, CROP_SIZE, CROP_SIZE):new():fill(1.0)
     local xOffset = math.floor((CROP_SIZE-w)/2)
@@ -101,11 +108,4 @@ function normalizeImage(img)
     nimg[{1, {1+yOffset, h+yOffset}, {1+xOffset,w+xOffset}}] = cimg
     return nimg
 end
-
-img = image.load[[/media/marat/MySSD/plankton/train/copepod_cyclopoid_copilia/30556.jpg]]
---img = image.load[[/media/marat/MySSD/plankton/train/acantharia_protist/79447.jpg]]
-d(img)
-d(normalizeImage(img))
-d(img)
-
 
