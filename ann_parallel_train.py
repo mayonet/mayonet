ae68a8d0f2feee7bcc328868e43eeb46b82189ea
yoptar <yoptar@gmail.com>
2015-03-10 18:56:57 +0300

    Last fat network

--- ann_parallel_train.py
+++ ann_parallel_train.py
@@ -39,8 +39,8 @@ np.random.seed(1100)
 dtype_y = 'uint8'
 model_fn = 'last_model_80_full.pkl'
 
-img_size = 80
-max_offset = 1
+img_size = 78
+max_offset = 0
 window = (img_size-2*max_offset, img_size-2*max_offset)
 
 train_x, train_y, train_names = load_npys(which_set='train', image_size=img_size)
@@ -66,30 +66,30 @@ cropped_window = (cropped_size-2*cropped_offset, cropped_size-2*cropped_offset)
 # csv_location = '/home/yoptar/git/subway-plankton/train_img_props.csv'
 # pan_props = pd.read_csv(csv_location, sep='\t')
 
-# train_props = None
-# for name in train_names:
-#     prop = np.cast[floatX](pan_props[pan_props['file_name'] == name].drop(['class', 'file_name'], axis=1).values)
-#     if train_props is None:
-#         train_props = prop
-#     else:
-#         train_props = np.vstack((train_props, prop))
-# print('prepared train_props')
-train_props = np.load('/plankton/train_props.npy')
+# # train_props = None
+# # for name in train_names:
+# #     prop = np.cast[floatX](pan_props[pan_props['file_name'] == name].drop(['class', 'file_name'], axis=1).values)
+# #     if train_props is None:
+# #         train_props = prop
+# #     else:
+# #         train_props = np.vstack((train_props, prop))
+# # print('prepared train_props')
+# train_props = np.load('/plankton/train_props.npy')
+#
+# # valid_props = None
+# # for name in valid_names:
+# #     prop = np.cast[floatX](pan_props[pan_props['file_name'] == name].drop(['class', 'file_name'], axis=1).values)
+# #     if valid_props is None:
+# #         valid_props = prop
+# #     else:
+# #         valid_props = np.vstack((valid_props, prop))
+# # print('prepared valid_props')
+# valid_props = np.load('/plankton/valid_props.npy')
 
-# valid_props = None
-# for name in valid_names:
-#     prop = np.cast[floatX](pan_props[pan_props['file_name'] == name].drop(['class', 'file_name'], axis=1).values)
-#     if valid_props is None:
-#         valid_props = prop
-#     else:
-#         valid_props = np.vstack((valid_props, prop))
-# print('prepared valid_props')
-valid_props = np.load('/plankton/valid_props.npy')
-
-means = train_props.mean(axis=0)
-stds = np.std(train_props, axis=0)
-train_props = (train_props - means) / stds
-valid_props = (valid_props - means) / stds
+# means = train_props.mean(axis=0)
+# stds = np.std(train_props, axis=0)
+# train_props = (train_props - means) / stds
+# valid_props = (valid_props - means) / stds
 
 len_in = train_x.shape[1]
 len_out = train_y.shape[1]
@@ -103,17 +103,17 @@ n_classes = len(unique_labels)
 label_to_int = {unique_labels[i]: i for i in range(n_classes)}
 
 
-def heat_ys(y):
-    cl = zip(0.7**np.arange(1, 10), np.exp(np.arange(2, 11)))
-    heat_ys.iter = min(heat_ys.iter, len(cl)-1)
-    res = heated_targetings(label_to_int, y,
-                            cl[heat_ys.iter][0], cl[heat_ys.iter][0], cl[heat_ys.iter][1])
-    heat_ys.counter += 1
-    if heat_ys.counter % 3 == 0:
-        heat_ys.iter += 1
-    return res
-heat_ys.counter = 0
-heat_ys.iter = 0
+# def heat_ys(y):
+#     cl = zip(0.7**np.arange(1, 10), np.exp(np.arange(2, 11)))
+#     heat_ys.iter = min(heat_ys.iter, len(cl)-1)
+#     res = heated_targetings(label_to_int, y,
+#                             cl[heat_ys.iter][0], cl[heat_ys.iter][0], cl[heat_ys.iter][1])
+#     heat_ys.counter += 1
+#     if heat_ys.counter % 3 == 0:
+#         heat_ys.iter += 1
+#     return res
+# heat_ys.counter = 0
+# heat_ys.iter = 0
 
 cost = neg_log_likelihood
 valid_cost = cost
@@ -130,60 +130,24 @@ else:
     mlp = MLP([
         # Parallel([
             MLP([
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 64, leaky_relu_alpha=1),
+                GaussianDropout(0.003),
+                ConvolutionalLayer((5, 5), 128, pad=1, leaky_relu_alpha=1),
                 MaxPool((2, 2)),
                 PReLU(prelu_alpha),
 
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 128, pad=1, leaky_relu_alpha=prelu_alpha),
+                # GaussianDropout(0.03),
+                ConvolutionalLayer((3, 3), 196, leaky_relu_alpha=prelu_alpha),
                 # MaxPool((2, 2)),
-                PReLU(prelu_alpha),
+                NonLinearity(),
 
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 128, leaky_relu_alpha=prelu_alpha),
+                # GaussianDropout(0.03),
+                ConvolutionalLayer((3, 3), 196, pad=1),
                 MaxPool((2, 2)),
                 NonLinearity(),
 
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256, pad=1),
-                # MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256, pad=1),
-                # MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
+                # GaussianDropout(0.03),
                 ConvolutionalLayer((3, 3), 256),
                 MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256, pad=1),
-                # MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256, pad=1),
-                # MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256, pad=1),
-                # MaxPool((2, 2)),
-                NonLinearity(),
-
-                GaussianDropout(0.03),
-                ConvolutionalLayer((3, 3), 256),
-                MaxPool((2, 2)),
-                NonLinearity(),
-
-                Flatten(),
-
-                Dropout(0.5),
-                DenseLayer(2000),
                 NonLinearity()
             ], logger=logger),
             # MLP([
@@ -218,32 +182,53 @@ else:
             # ])
         # ]),
 
-        Dropout(0.5),
-        DenseLayer(2000),
-        NonLinearity(),
+        MLP([
+            MaxPool((2, 2)),
+            # # GaussianDropout(0.03),
+            # ConvolutionalLayer((3, 3), 256, pad=1),
+            # MaxPool((2, 2)),
+            # NonLinearity(),
+            #
+            # # GaussianDropout(0.03),
+            # ConvolutionalLayer((3, 3), 256),
+            # MaxPool((2, 2)),
+            # NonLinearity(),
 
-        Dropout(0.5),
-        DenseLayer(len_out),
-        NonLinearity(activation=T.nnet.softmax)
+            Flatten(),
+
+            # Dropout(0.5),
+            DenseLayer(2000),
+            NonLinearity()
+        ], logger=logger),
+
+        MLP([
+            Dropout(0.5),
+            DenseLayer(2000),
+            NonLinearity(),
+
+            Dropout(0.5),
+            DenseLayer(len_out),
+            NonLinearity(activation=T.nnet.softmax)
+        ], logger=logger),
     ], (1,) + window,  # (1,) + cropped_window  # , train_props.shape[1:]
         logger=logger)
 
 
 
 ## TODO move to mlp.get_updates
-l2 = 0.000001  # 1e-5
-learning_rate = 2e-2  # np.exp(-2)
+l2 = 0  # .000001  # 1e-5
+learning_rate = 3e-5  # np.exp(-2)
 momentum = 0.97
 epoch_count = 1000
-batch_size = 64
+batch_size = 100
 minibatch_count = train_y.shape[0] // batch_size
-learning_decay = 1  # 0.5 ** (1./(300 * minibatch_count))
+learning_decay = 1  # 0.5 ** (1./(10 * minibatch_count))
 momentum_decay = 1  # 0.5 ** (1./(1000 * minibatch_count))
 lr_min = 1e-15
 mm_min = 0.5
 valid_rnd_count = 1
 
-method = 'adadelta'
+method = 'nesterov'
 
 logger.write('batch=%d, l2=%f, method=%s\nlr=%f, lr_decay=%f,\nmomentum=%f, momentum_decay=%f' %
              (batch_size, l2, method, learning_rate, learning_decay, momentum, momentum_decay))
@@ -277,7 +262,7 @@ def randomize(dataset):
 
 tr = Trainer(mlp, batch_size, learning_rate, train_x, train_y, valid_X=valid_x, valid_y=valid_y, method=method,
              momentum=momentum, lr_decay=learning_decay, lr_min=lr_min, l2=l2, mm_decay=momentum_decay, mm_min=mm_min,
-             train_augmentation=randomize, valid_augmentation=randomize, valid_aug_count=valid_rnd_count,
+             # train_augmentation=randomize, valid_augmentation=randomize, valid_aug_count=valid_rnd_count,
              train_y_augmentation=train_y_modifier,
              model_file_name=model_fn, save_freq=1, save_in_different_files=True, epoch_count=epoch_count,
              cost_f=cost, valid_cost_f=valid_cost)
