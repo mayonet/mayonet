a4949e0a6ba05fef477f78cc959005803303ee7b
Marat Zaynutdinov <tsundokum@gmail.com>
2015-03-16 03:24:23 +0300

    amazon_5x5resizelarger.py

--- amazon_5x5resizelarger.py
+++ amazon_5x5resizelarger.py
@@ -37,7 +37,7 @@ logger = Logger(logger_name)
 np.random.seed(1147)
 
 dtype_y = 'uint8'
-model_fn = 'amazon_5x5shrink.pkl'
+model_fn = 'amazon_5x5resizelarger.pkl'
 
 img_size = 128
 max_offset = 10
